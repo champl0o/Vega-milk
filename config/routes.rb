e0c014f1f840244b807ed968b6e8e51c019ad70e
Nazar Matus <funkyloverone@gmail.com>
2021-04-19 21:42:33 +0300

    Blank Rails 6.1 app with Postgres DB

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+end
