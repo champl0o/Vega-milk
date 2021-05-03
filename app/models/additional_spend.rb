cd31f7d48f06ecbf143959f07c84e1f9e549ba69
Nazar Matus <funkyloverone@gmail.com>
2021-05-03 23:32:52 +0300

    AdditionalSpend: add validations, scopes, FactoryBot

--- app/models/additional_spend.rb
+++ app/models/additional_spend.rb
@@ -1,2 +1,11 @@
 class AdditionalSpend < ApplicationRecord
+  scope :in_date_range, ->(range) { where(arel_table[:date].between(range)) }
+  scope :order_by_date, ->(direction = :asc) { order(date: direction) }
+
+  validates :title, :date, presence: true
+  validates :cost_in_cents, numericality: { greater_than: 0 }
+
+  def cost
+    BigDecimal(cost_in_cents) / 100
+  end
 end
