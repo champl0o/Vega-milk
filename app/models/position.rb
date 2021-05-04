97b3c3a3c55a5e6b04c9d5ebfedaaa3124663360
champlOo <podilkaroman@gmail.com>
2021-05-05 00:28:02 +0300

    Salary-branch: added salary_report view, upgraded seeds, created position and employee factories

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Position < ApplicationRecord
+  has_many :employees
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -8,4 +8,18 @@ FactoryBot.define do
     cost_in_cents { rand(100_00..10_000_00) }
     date { Faker::Date.between(from: 2.months.ago, to: Date.today) }
   end
+
+  factory :position do
+    name { Faker::Job.title }
+    salary_in_cents { rand(1_000_00..10_000_00) }
+  end
+
+  factory :employee do
+    position { Position.find(rand(1..(Position.all.count - 1))) }
+    name { Faker::FunnyName.name }
+    email { Faker::Internet.email }
+    phone { Faker::PhoneNumber.phone_number }
+    begin_date { Faker::Date.between(from: 2.months.ago, to: Date.today) }
+    end_date { rand(1..2) == 1 ? Faker::Date.between(from: begin_date, to: Date.today) : nil }
+  end
 end
