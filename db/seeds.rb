99ea6339df499f277ce7876df513145c25f09fab
champlOo <podilkaroman@gmail.com>
2021-05-06 16:16:56 +0300

    improved income report

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require 'faker'
-require 'factory_bot_rails'
+# require 'faker'
+# require 'factory_bot_rails'
 
-pos = Position.create(name: 'Worker', salary_in_cents: 125_00)
-e = Employee.create(
-  position: pos,
-  name: 'Olive Branch',
-  email: 'donald.rowe@tremblay-hettinger.name',
-  phone: '613.027.7507 x1872',
-  begin_date: DateTime.parse('04/04/2021')
-)
-u = User.create!(
-  email: 'test@example.com',
-  password: 'password',
-  password_confirmation: 'password',
-  role: :admin,
-  employee: e
-)
+# pos = Position.create(name: 'Worker', salary_in_cents: 125_00)
+# e = Employee.create(
+#   position: pos,
+#   name: 'Olive Branch',
+#   email: 'donald.rowe@tremblay-hettinger.name',
+#   phone: '613.027.7507 x1872',
+#   begin_date: DateTime.parse('04/04/2021')
+# )
+# u = User.create!(
+#   email: 'test@example.com',
+#   password: 'password',
+#   password_confirmation: 'password',
+#   role: :admin,
+#   employee: e
+# )
 
-100.times { FactoryBot.create(:additional_spend) }
-100.times { FactoryBot.create(:employee) }
-100.times { FactoryBot.create(:bought_ingredient) }
-100.times { FactoryBot.create(:product_ingredient) }
-100.times { FactoryBot.create(:batch) }
-100.times { FactoryBot.create(:order) }
+# 100.times { FactoryBot.create(:additional_spend) }
+# 100.times { FactoryBot.create(:employee) }
+# 100.times { FactoryBot.create(:bought_ingredient) }
+# 100.times { FactoryBot.create(:product_ingredient) }
+# 100.times { FactoryBot.create(:batch) }
+400.times { FactoryBot.create(:order) }
 
 puts 'Awesome'
